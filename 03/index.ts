d6f8e91a0bc525a209ab13e25b615f8736e14bdb
Olavi Haapala <olavi.haapala@futurice.com>
2018-12-03 08:10:43 +0200

    Solve day 03 part 1

---
+++ 03/index.ts
@@ -0,0 +1,101 @@
+const fs = require('fs');
+
+interface Claim {
+  id: number;
+  offset: {
+    left: number;
+    top: number;
+  };
+  size: {
+    width: number;
+    height: number;
+  };
+}
+
+interface Square {
+  x: number;
+  y: number;
+}
+
+const parseInputRow = str => {
+  // Example #728 @ 503,489: 20x23
+  const id = Number(str.match(/(?<=\#)\d*/g));
+  const left = Number(str.match(/(?<=\@\s)\d*/g));
+  const top = Number(str.match(/(?<=\@\s\d*\,)\d*/g));
+  const width = Number(str.match(/(?<=\:\s)\d*/g));
+  const height = Number(str.match(/(?<=\:\s\d*\x)\d*/g));
+
+  const claim: Claim = {
+    id,
+    offset: {
+      left,
+      top
+    },
+    size: {
+      width,
+      height
+    }
+  };
+  return claim;
+};
+
+const createClaimMap = (claims: Claim[]) => {
+  const map = new Map<string, number>();
+
+  claims.forEach(claim => {
+    for (
+      let x = claim.offset.left;
+      x < claim.offset.left + claim.size.width;
+      x++
+    ) {
+      for (
+        let y = claim.offset.top;
+        y < claim.offset.top + claim.size.height;
+        y++
+      ) {
+        const square: Square = { x, y };
+        if (map.get(`${square.x},${square.y}`)) {
+          map.set(
+            `${square.x},${square.y}`,
+            map.get(`${square.x},${square.y}`) + 1
+          );
+        } else {
+          map.set(`${square.x},${square.y}`, 1);
+        }
+      }
+    }
+  });
+
+  return map;
+};
+
+const hasBeenClaimedTwiceOrMore = occurenceMap => {
+  const result: string[] = [];
+  for (var [key, value] of occurenceMap.entries()) {
+    if (value > 1) {
+      result.push(key);
+    }
+  }
+  return result;
+};
+
+const day03part1 = input => {
+  const claims = input.map(parseInputRow);
+  const claimMap = createClaimMap(claims);
+  const doubleClaimed = hasBeenClaimedTwiceOrMore(claimMap);
+  return doubleClaimed.length;
+};
+
+const run03 = async () => {
+  console.log('Starting up');
+  await fs.readFile('./inputs/03.txt', 'utf8', (err, contents) => {
+    if (err) {
+      console.error('ERROR', err);
+    }
+
+    const splittedInput: string[] = contents.split('\n').filter(x => x !== '');
+    console.log(day03part1(splittedInput));
+  });
+};
+
+run03();
